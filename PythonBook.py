84e1abf6b4d4f926109a142dc026b6d58e057e0b
n-key <48398042+n-key@users.noreply.github.com>
2019-06-05 15:13:07 +1000

    Add files via upload

---
+++ PythonBook.py
@@ -0,0 +1,42 @@
+import urllib
+import webbrowser
+import os #alteration
+import time
+
+url = "https://www.codecademy.com/learn/learn-python" #change the addr to target website
+url_1 = "http://www.tutorialspoint.com/python/" #also make sure the http/s/ is correct
+url_2 = "https://www.codementor.io/community/topic/python"
+url_3 = "http://www.pythonchallenge.com/"
+url_4 = "https://docs.python.org/3/tutorial/index.html"
+url_5 = "https://learnpythonthehardway.org/"
+url_6 = "https://www.learnpython.org/"
+url_7 = "http://greenteapress.com/thinkpython/html/index.html"
+url_8 = "https://docs.python-guide.org/"
+url_9 = "https://stackoverflow.com/questions/tagged/python"
+
+chrome_path = ("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
+
+os.startfile("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "open")
+
+time.sleep(2)
+
+webbrowser.get(chrome_path).open(url)
+#webbrowser.get(chrome_path).open(url + '/doc')
+webbrowser.get(chrome_path).open_new_tab(url_1)
+#webbrowser.get(chrome_path).open(url_1 + '/doc')
+webbrowser.get(chrome_path).open_new_tab(url_2)
+#webbrowser.get(chrome_path).open(url_2 + '/doc')
+webbrowser.get(chrome_path).open_new_tab(url_3)
+#webbrowser.get(chrome_path).open(url_3 + '/doc')
+webbrowser.get(chrome_path).open_new(url_4)
+#webbrowser.get(chrome_path).open(url_4 + '/doc')
+webbrowser.get(chrome_path).open_new(url_5)
+webbrowser.get(chrome_path).open_new(url_6)
+webbrowser.get(chrome_path).open_new(url_7)
+webbrowser.get(chrome_path).open_new(url_8)
+webbrowser.get(chrome_path).open_new(url_9)
+
+"""not sure if needed"""
+""" info about the code """
+
+
